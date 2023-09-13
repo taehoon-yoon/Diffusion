7b53de6cb514795f9abdea2897041cc25ab9326c
TaeHooon <willyepep@naver.com>
2023-09-13 20:07:24 +0900

    finished inference code

---
+++ inference.py
@@ -0,0 +1,43 @@
+from src import model_torch
+from src import model_original
+from src.diffusion import GaussianDiffusion, DDIM_Sampler
+from src.inferencer import Inferencer
+import yaml
+import argparse
+
+
+def main(args):
+    with open(args.config, 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    unet_cfg = config['unet']
+    ddim_cfg = config['ddim']
+    trainer_cfg = config['inferencer']
+    image_size = unet_cfg['image_size']
+
+    if config['type'] == 'original':
+        unet = model_original.Unet(**unet_cfg).to(args.device)
+    elif config['type'] == 'torch':
+        unet = model_torch.Unet(**unet_cfg).to(args.device)
+    else:
+        unet = None
+        print("Unet type must be one of ['original', 'torch']")
+        exit()
+
+    diffusion = GaussianDiffusion(unet, image_size=image_size).to(args.device)
+
+    ddim_samplers = list()
+    for sampler_cfg in ddim_cfg.values():
+        ddim_samplers.append(DDIM_Sampler(diffusion, **sampler_cfg))
+
+    inferencer = Inferencer(diffusion, ddim_samplers=ddim_samplers, time_step=diffusion.time_step, **trainer_cfg)
+    inferencer.load(args.load)
+    inferencer.inference()
+
+
+if __name__ == '__main__':
+    parse = argparse.ArgumentParser(description='DDPM & DDIM')
+    parse.add_argument('-c', '--config', type=str, default='./config/inference/cifar10.yaml')
+    parse.add_argument('-l', '--load', type=str, default=None)
+    parse.add_argument('-d', '--device', type=str, choices=['cuda', 'cpu'], default='cuda')
+    args = parse.parse_args()
+    main(args)
--- src/inferencer.py
+++ src/inferencer.py
@@ -1,5 +1,7 @@
 import os
 import math
+
+import torch
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
 from .dataset import dataset_wrapper
@@ -13,14 +15,15 @@ from imageio import mimsave
 
 class Inferencer:
     def __init__(self, diffusion_model, dataset, ddim_samplers=None, batch_size=32, num_samples_per_image=25,
-                 result_folder='./inference_results', num_images_to_generate=1, ddpm_fid_estimate=True,
-                 ddpm_num_fid_samples=None, clip=True, return_all_step=True, make_denoising_gif=True, gif_duration=3):
+                 result_folder='./inference_results', num_images_to_generate=1, ddpm_fid_estimate=True, time_step=1000,
+                 ddpm_num_fid_samples=None, clip=True, return_all_step=True, make_denoising_gif=True, num_gif=50):
         dataset_name = os.path.basename(dataset)
         if dataset_name == '':
             dataset_name=os.path.basename(os.path.dirname(dataset))
         self.diffusion_model = diffusion_model
         self.ddim_samplers = ddim_samplers
         self.batch_size = batch_size
+        self.time_step = time_step
         self.num_samples = num_samples_per_image
         self.num_images = num_images_to_generate
         self.nrow = int(math.sqrt(self.num_samples))
@@ -31,7 +34,7 @@ class Inferencer:
         self.device = self.diffusion_model.device
         self.return_all_step = return_all_step or make_denoising_gif
         self.make_denoising_gif = make_denoising_gif
-        self.gif_duration = gif_duration
+        self.num_gif = num_gif
         self.clip = clip
         self.ddpm_fid_flag = ddpm_fid_estimate
         self.cal_fid = True if self.ddpm_fid_flag else False
@@ -59,11 +62,9 @@ class Inferencer:
                     os.makedirs(os.path.join(save_path, 'clip'), exist_ok=True)
                 if sampler.clip is False or sampler.clip == 'both':
                     os.makedirs(os.path.join(save_path, 'no_clip'), exist_ok=True)
-                if sampler.calculate_fid:
-                    self.cal_fid = True
-                    sampler.num_fid_sample = sampler.num_fid_sample if sampler.num_fid_sample is not None else len(
-                        dataSet)
-                    self.fid_score_log[sampler.sampler_name] = list()
+            if sampler.calculate_fid:
+                self.cal_fid = True
+                sampler.num_fid_sample = sampler.num_fid_sample if sampler.num_fid_sample is not None else len(dataSet)
 
         # Image generation log
         notification = make_notification('Image Generation', color='light_cyan')
@@ -72,7 +73,7 @@ class Inferencer:
         print(colored('-> DDPM Sampler', 'light_cyan'))
         for sampler in self.ddim_samplers:
             if sampler.generate_image:
-                print(colored('-> {}', 'light_cyan'))
+                print(colored('-> {}'.format(sampler.sampler_name), 'light_cyan'))
         print('\n')
 
         # FID score
@@ -101,7 +102,7 @@ class Inferencer:
     def inference(self):
         notification = make_notification('Inferencing', color='light_yellow', boundary='+')
         print(notification)
-        print(colored('Image Generation', 'light_yellow'))
+        print(colored('Image Generation\n', 'light_yellow'))
 
         # DDPM sampler
         for idx in tqdm(range(self.num_images), desc='DDPM image sampling'):
@@ -109,16 +110,78 @@ class Inferencer:
             for i, j in zip([True, False], ['clip', 'no_clip']):
                 if self.clip not in [i, 'both']:
                     continue
-                imgs = list(map(lambda n: self.diffusion_model.sample(batch_size=n, clip=i,
-                                                                      return_all_timestep=self.return_all_step), batches))
+                imgs = list(map(lambda n: self.diffusion_model.sample(n, self.return_all_step, clip=i), batches))
                 imgs = torch.cat(imgs, dim=0)  # (batch, steps, ch, h, w)
                 if self.return_all_step:
-                    path = os.path.join(self.ddpm_result_folder, 'j', '{}'.format(idx + 1))
-                    os.makedirs(path)
+                    path = os.path.join(self.ddpm_result_folder, '{}'.format(j), '{}'.format(idx + 1))
+                    os.makedirs(path, exist_ok=True)
                     for step in range(imgs.shape[1]):
                         save_image(imgs[:, step], nrow=self.nrow, fp=os.path.join(path, '{:04d}.png'.format(step)))
                     if self.make_denoising_gif:
-                        file_names = sorted(glob(os.path.join(path, '*.png')))
+                        gif_step = int(self.time_step/self.num_gif)
+                        gif_step = max(1, gif_step)
+                        file_names = list(reversed(sorted(glob(os.path.join(path, '*.png')))))[::gif_step]
+                        file_names = reversed(file_names)
                         gif = [imageio.v2.imread(names) for names in file_names]
-                        mimsave('{}_{}.gif'.format(idx+1, j), gif, **{'duration': self.gif_duration/imgs.shape[1]})
+                        mimsave(os.path.join(self.ddpm_result_folder, '{}_{}.gif'.format(idx+1, j)),
+                                gif, **{'duration': self.num_gif / imgs.shape[1]})
+                last_img = imgs[:,-1] if self.return_all_step else imgs
+                save_image(last_img, nrow=self.nrow,
+                           fp=os.path.join(self.ddpm_result_folder, '{}_{}.png'.format(idx+1, j)))
+        # DDIM sampler
+        for sampler in self.ddim_samplers:
+            if sampler.generate_image:
+                for idx in tqdm(range(self.num_images), desc='{} image sampling'.format(sampler.sampler_name)):
+                    batches = num_to_groups(self.num_samples, self.batch_size)
+                    for i, j in zip([True, False], ['clip', 'no_clip']):
+                        if sampler.clip not in [i, 'both']:
+                            continue
+                        imgs = list(map(lambda n: sampler.sample(n, None, self.return_all_step, clip=i), batches))
+                        imgs = torch.cat(imgs, dim=0)
+                        if self.return_all_step:
+                            path = os.path.join(sampler.save_path, '{}'.format(j), '{}'.format(idx + 1))
+                            os.makedirs(path, exist_ok=True)
+                            for step in range(imgs.shape[1]):
+                                save_image(imgs[:, step], nrow=self.nrow,
+                                           fp=os.path.join(path, '{:04d}.png'.format(step)))
+                            if self.make_denoising_gif:
+                                gif_step = int(sampler.ddim_steps/self.num_gif)
+                                gif_step = max(1, gif_step)
+                                file_names = list(reversed(sorted(glob(os.path.join(path, '*.png')))))[::gif_step]
+                                file_names = reversed(file_names)
+                                gif = [imageio.v2.imread(names) for names in file_names]
+                                mimsave(os.path.join(sampler.save_path, '{}_{}.gif'.format(idx + 1, j)),
+                                        gif, **{'duration': self.num_gif / imgs.shape[1]})
+                        last_img = imgs[:, -1] if self.return_all_step else imgs
+                        save_image(last_img, nrow=self.nrow,
+                                   fp=os.path.join(sampler.save_path, '{}_{}.png'.format(idx + 1, j)))
+        if self.cal_fid:
+            print(colored('\nFID score estimation\n', 'light_yellow'))
+            if self.ddpm_fid_flag:
+                print(colored('DDPM FID calculation...', 'yellow'))
+                ddpm_fid = self.fid_scorer.fid_score(self.diffusion_model.sample, self.ddpm_num_fid_samples)
+                self.fid_score_log['DDPM'] = ddpm_fid
+            for sampler in self.ddim_samplers:
+                print(colored('{} FID calculation...'.format(sampler.sampler_name), 'yellow'))
+                if sampler.calculate_fid:
+                    ddim_fid = self.fid_scorer.fid_score(sampler.sample, sampler.num_fid_sample)
+                    self.fid_score_log[f'{sampler.sampler_name}'] = ddim_fid
+            print(colored('-'*50, 'yellow'))
+            for key, val in self.fid_score_log.items():
+                print(colored('Sampler: {} -> FID score: {}'.format(key, val), 'yellow'))
+            with open(os.path.join(self.result_folder, 'FID.txt'), 'w') as f:
+                f.write('Results\n')
+                f.write('='*50)
+                f.write('\n')
+                for key, val in self.fid_score_log.items():
+                    f.write('Sampler: {} -> FID score: {}\n'.format(key, val))
 
+    def load(self, path):
+        if not os.path.exists(path):
+            print(make_notification('ERROR', color='red', boundary='*'))
+            print(colored('No saved checkpoint is detected. Please check you gave existing path!', 'red'))
+            exit()
+        print(make_notification('Loading Checkpoint', color='green'))
+        data = torch.load(path, map_location=self.device)
+        self.diffusion_model.load_state_dict(data['model'])
+        print(colored('Successfully loaded checkpoint!\n', 'green'))
